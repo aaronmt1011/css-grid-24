99d4d18af50f49a428a872a6af357cda5dc923e2
aaronmt1011 <aaronmt1011@gmail.com>
2023-02-28 09:23:12 -0700

    Created basic image gallery

--- 19-CSS_Grid_Image_Gallery/script.js
+++ 19-CSS_Grid_Image_Gallery/script.js
@@ -4,17 +4,19 @@ const overlayImage = overlay.querySelector('img');
 const overlayClose = overlay.querySelector('.close');
 
 
-// generateHTML() is used to create the image overlay
+
+// generateHTML() is used to create a single image overlay
 function generateHTML([h, v]) {
     return `
-        <div class = "item h${h} v${v}
-            <img src="images/${randomNumber(12)}.jpg>
+        <div class = "item h${h} v${v}">
+            <img src="images/${randomNumber(12)}.jpg">
             <div class="item__overlay">
-                <button>View -></button>
+                <button>View →</button>
             </div>
         </div>
     `;
 }
+
 
 
 // randomNumber() gives out a random number that is used in the function above
@@ -22,3 +24,18 @@ function generateHTML([h, v]) {
 function randomNumber(limit) {
     return Math.floor(Math.random() * limit) + 1;
 }
+
+
+
+// Array digits is created with a length of 50 indexes and uses .from() function
+// to fill up the 50 slots with an array that that has 2 random numbers 
+// between 1-4.  
+const digits = Array.from({length: 50}, () => 
+    [randomNumber(4), randomNumber(4)]);
+
+
+
+// the html var uses .map to put each array index through the generateHTML().
+// .join() is then used to convert to a string.
+const html = digits.map(generateHTML).join('');
+gallery.innerHTML = html;
